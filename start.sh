84756dd5805e80738f38ea77c92f75c5fea9dd48
AB <ab@h.com>
2023-01-02 10:40:14 +0100

    add start script

---
+++ start.sh
@@ -0,0 +1,4 @@
+source venv/bin/activate
+python main.py -f config.ini > alexanderD_Beck.log &
+python main.py -f config2.ini > theInnovationG3.log &
+python main.py -f config3.ini > iceceubelytics.log &
