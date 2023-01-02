53bc96d68aa23ce6084829ec287e621671388f50
AB <ab@h.com>
2023-01-02 10:17:23 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='twitterBot',
+    version='',
+    packages=['twitterBot'],
+    url='',
+    license='',
+    author='alex',
+    author_email='',
+    description=''
+)
