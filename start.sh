c107cbea98db57c8cc73a9454c46d6bf09f69a5a
AB <ab@h.com>
2023-01-09 20:30:39 +0100

    changed start.sh back

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 screen -X -S c1 quit
 screen -X -S c2 quit
 screen -X -S c3 quit
-screen -dmS "c1" sh -c ". venv/bin/activate; python main.py -f config.ini  &> c1.log"
-screen -dmS "c2" sh -c ". venv/bin/activate; python main.py -f config2.ini &> c2.log"
-screen -dmS "c3" sh -c ". venv/bin/activate; python main.py -f config3.ini &> c3.log"
+screen -dmS "c1" sh -c ". venv/bin/activate; python main.py -f config.ini"
+screen -dmS "c2" sh -c ". venv/bin/activate; python main.py -f config2.ini"
+screen -dmS "c3" sh -c ". venv/bin/activate; python main.py -f config3.ini"
